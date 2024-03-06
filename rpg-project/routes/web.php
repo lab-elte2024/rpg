b07bf27316e0681108822423ee102b106997b7dc
olaszakos19 <akos.olasz17@gmail.com>
2024-03-06 16:16:55 +0100

    pages

--- rpg-project/routes/web.php
+++ rpg-project/routes/web.php
@@ -2,16 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,3 +11,10 @@ Route::get('/', function () {
 
 Route::get('/weapons', [GameController::class, 'index']);
 
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
