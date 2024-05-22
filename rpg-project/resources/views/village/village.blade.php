477f7e2d352f89075f7e2c18dd150ea751d7e226
nblint <62309192+HarDeneD1@users.noreply.github.com>
2024-05-22 16:02:05 +0200

    kiscss

--- rpg-project/resources/views/village/village.blade.php
+++ rpg-project/resources/views/village/village.blade.php
@@ -12,11 +12,11 @@
 </head>
 <body>
     <div class="parent">
-        <div class="div1" id="blacksmith"><a href="blacksmith">blacksmith</a></div>
-        <div class="div2" id ="tavern"> tavern</div>
-        <div class="div3" id="mission"><a href="mission">quests</a></div>
-        <div class="div4" id = "battle"><a href="battle">test</a></div>
-        <div class="div5" id= "stat"><a href="stat">player stats</a></div>
+        <div class="div1" id="blacksmith"><a href="blacksmith"> <h3>blacksmith</h3></a></div>
+        <div class="div2" id ="tavern"> <h3>tavern</h3></div>
+        <div class="div3" id="mission"><a href="mission"><h3>quests</h3></a></div>
+        <div class="div4" id = "battle"><a href="battle"><h3>test</h3></a></div>
+        <div class="div5" id= "stat"><a href="stat"><h3>player stats</h3></a></div>
         </div>
 </body>
 </html>
