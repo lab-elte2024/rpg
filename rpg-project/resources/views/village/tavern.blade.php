ef60496f59d4a5f2cb4b1ba2a4b9a41d30e4cef0
Orbán László <115794585+Lacikaki7@users.noreply.github.com>
2024-05-30 21:24:37 +0200

    tavern update

---
+++ rpg-project/resources/views/village/tavern.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset('css/mainstyle.css')}}" rel= "stylesheet">
+    <link href="{{ asset('css/tavern.css') }}" rel="stylesheet">
+
+
+    <title>Tavern</title>
+
+
+
+</head>
+<body>
+
+</body>
+    <div class="parent">
+        <div class="div1">
+            <button><h1>Sör</h1></button>
+        </div>
+        <div class="div2">
+            <button><a href="logic"><h1>JÁTÉKGÉP</h1></a></button>
+        </div>
+        <div class="div3">
+            <a href = "/village" >Vissza</a>
+        </div>
+        
+    </div>
+    
+</html>
