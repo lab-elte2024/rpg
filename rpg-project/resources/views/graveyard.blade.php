de3e3bbd2b1109f296a26319acbb1f994a5a4d64
Orbán László <115794585+Lacikaki7@users.noreply.github.com>
2024-05-31 18:57:34 +0200

    Graveyard update

---
+++ rpg-project/resources/views/graveyard.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset('css/mainstyle.css')}}" rel= "stylesheet">
+    <link href="{{ asset('css/graveyard.css') }}" rel="stylesheet">
+    <title>Graveyard</title>
+</head>
+<body>
+    <div class="parent">
+        <div class="div1">Meghaltál!</div>
+        <div class="div2"><button><a href="">Új játék</a></button></div>
+    </div>
+</html>
